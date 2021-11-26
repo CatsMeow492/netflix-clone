cbbb4863424bfae1aac69b93357f7592664e6aef
Catsmeow492 <taylormohney@icloud.com>
2021-11-26 15:44:43 -0500

    taylor-index

--- pages/index.js
+++ pages/index.js
@@ -9,12 +9,13 @@ export default function Home() {
       </Head>
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-      <h1 className="text-5xl font-bold">Unlimited movies, TV shows, and more.</h1>
-      <h2 className="text-2xl">Watch anywhere. Cancel Anytime.</h2><br></br>
-      <p className="text-1xl">Ready to watch? Enter your email to create or restart your membership</p>
-      <form>
-      <input type="email"></input>
-      </form>
+        <h1 className="text-5xl font-bold">Unlimited movies, TV shows, and more.</h1>
+        <h2 className="text-2xl">Watch anywhere. Cancel Anytime.</h2><br></br>
+        <p className="text-1xl">Ready to watch? Enter your email to create or restart your membership</p>
+      <div class="mb-4">
+        <input class="shadow appearance-none border rounded w-full py-2 px-10 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="text" placeholder="email">
+        </input>
+       </div>
       </main>
     </div>
   )
